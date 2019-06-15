d8ec5854f8c08ae6930e67f2ec9316352c6b07a3
Akaradech Sutheesuntornkul <iamakaradech@gmail.com>
2019-06-15 22:58:39 +0700

    add README.md and initial script

---
+++ README.md
@@ -0,0 +1,10 @@
+# Installation step
+```
+docker-compose build && docker-compose up -d
+
+### after service started ###
+
+chmod +x init.sh
+
+./init.sh
+```
---
+++ init.sh
@@ -0,0 +1,4 @@
+echo "### Migrate data ###"
+docker exec -it laravel-app php artisan migrate
+echo "### Seed data ###"
+docker exec -it laravel-app php artisan db:seed --class=UserTableSeeder
